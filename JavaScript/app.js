ee5a45579981853cbb295200083163e7dd9b6fb6
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-09-03 01:50:53 +0600

    news cards are done somehow

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -6,18 +6,39 @@ const loadCategories = async () => {
 }
 
 const displayCategories = (categories) => {
-    console.log(categories);
+    // console.log(categories);
     const categoriesContainer = document.getElementById("categories-container");
     for (const category of categories) {
         const categoryDiv = document.createElement("div");
         categoryDiv.innerHTML = `
-            <h5>${category.category_name}</h5>
+            <h5 onclick="loadNews(${category.category_id})">${category.category_name}</h5>
         `
         categoriesContainer.appendChild(categoryDiv);
 
         // adding event handler for click on the categories
-        
     }
 }
+
+const loadNews = async (categoryId) => {
+    const addingZero = "0" + categoryId;
+    const url = `https://openapi.programming-hero.com/api/news/category/${addingZero}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayNews(data.data);
+}
+
+const displayNews = (news) => {
+    console.log(news);
+}
+
+
+
+
+
+
+
+
+
+
 
 loadCategories();

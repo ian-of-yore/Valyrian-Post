85162cb10367010d9f5fb36ae14ff52cb2bc749f
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-09-02 22:00:07 +0600

    categories added

---
+++ JavaScript/app.js
@@ -0,0 +1,22 @@
+const loadCategories = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCategories(data.data.news_category);
+}
+
+const displayCategories = (categories) => {
+    console.log(categories);
+    const categoriesContainer = document.getElementById("categories-container");
+    for (const category of categories) {
+        const categoryDiv = document.createElement("div");
+        categoryDiv.innerHTML = `
+            <h5>${category.category_name}</h5>
+        `
+        categoriesContainer.appendChild(categoryDiv);
+
+
+    }
+}
+
+loadCategories();
